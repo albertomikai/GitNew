9f44ccdec21ab01f2a6d8a753a3ae814c387992e
albertomikai <alberto.mikai@hm.com>
2021-02-05 09:35:54 +0100

    ÃChanges created new file

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        String shortestString = null;
+        // write your code here
+        List<String> movielist = List.of("Old Boy", "Shining", "Alien");
+        for (String tmp : movielist) {
+            if (shortestString == null) {
+                shortestString = tmp;
+                continue;
+            }
+            shortestString = shortestString.length() > tmp.length() ? tmp : shortestString;
+
+        }
+        System.out.println(shortestString);
+        System.out.println(shortestString);
+    }
+}
+
